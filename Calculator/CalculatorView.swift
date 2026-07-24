213b0457cdea05b8bba558472d4975577ad77c10
emmameowss <sam065819@outlook.com>
2026-07-24 21:45:56 +0200

    init

---
+++ Calculator/CalculatorView.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorView.swift
+//  Calculator
+//
+//
+
+import SwiftUI
+
+struct CalculatorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CalculatorView()
+}
